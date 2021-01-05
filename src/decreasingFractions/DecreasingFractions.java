df1373dbc6f4e94f565285879b0dcbba4e7ef7aa
Danzzilla <danielsvirida@gmail.com>
2021-01-05 13:52:08 -0800

    Initial commit

--- src/decreasingFractions/DecreasingFractions.java
+++ src/decreasingFractions/DecreasingFractions.java
@@ -11,21 +11,24 @@ public class DecreasingFractions {
 			double i = 1;
 			double sum = 0;
 			
-			System.out.print("1/"+ i);
 			while (sum < x){
 				sum = sum + (1/i);
 				//System.out.print(" "+sum);                              //switched between commenting out
-				System.out.print(" + "+"1/"+ (int)i);                     //these two to find bug
+				System.out.print("1/"+ (int)i);                           //these two to find error
 				i++;                                                      //the problem is it doesnt update the sum
-			}                                                             //solved when changed int i to a double instead of an int on line 11
-			System.out.println(" = "+sum);                                //the problem happened because on line 16 it divides an int by a
-			return sum;                                                   //larger int, causing it to equal 0, and causing the sum not to update
+				if((sum + (1/i)) <= x) {                                  //solved when changed int i to a double instead of an int on line 11
+					System.out.print(" + ");                              //the problem happened because on line 16 it divides an int by a
+				}                                                         //larger int, causing it to equal 0, and causing the sum not to update
+			}                                                             
+			System.out.println(" = "+sum);                                
+			return sum;                                                   
 		}
 	}
 	
 	public static void main(String[] args){
-	      System.out.println(sequenceSum(0.5)+"\n");
-	      System.out.println(sequenceSum(2.0)+"\n");
-	      System.out.println(sequenceSum(7.0)+"\n");
+		System.out.println(sequenceSum(0.5)+"\n");  
+		System.out.println(sequenceSum(1.5)+"\n");
+	    System.out.println(sequenceSum(2.0)+"\n");
+	   System.out.println(sequenceSum(7.0)+"\n");
 	}
 }
